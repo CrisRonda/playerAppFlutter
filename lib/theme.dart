69c91a28175e7fbf6bd55ffa116de6545beb28c9
Cristian Ronda <cristian.ronda@epn.edu.ec>
2019-01-26 18:12:35 -0500

    Agregador CustomClipper

---
+++ lib/theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color accentColor= Color(0xFFf08f8f);
+const Color lightAccentColor= Color(0xFFffafaf);
+const Color darkAccentColor= Color(0xFFd06f6f);
