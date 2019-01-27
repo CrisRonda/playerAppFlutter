e6821745c22d3811f9faccc317b080d9e0029e18
Cristian Ronda <cristian.ronda@epn.edu.ec>
2019-01-27 14:24:43 -0500

    Barra de busqueda y arrastrable agregada

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'bottom_controls.dart';
 import 'songs.dart';
 import 'theme.dart';
 import 'dart:math';
+import 'package:fluttery_dart2/gestures.dart';
 
 void main() => runApp(MyApp());
 
@@ -41,7 +42,9 @@ class _MyHomePageState extends State<MyHomePage> {
               Icons.menu,
             ),
             color: Color(0xffdddddd),
-            onPressed: () {},
+            onPressed: () {
+              print("holaMundo!");
+            },
           ),
         ],
       ),
@@ -49,30 +52,9 @@ class _MyHomePageState extends State<MyHomePage> {
         children: <Widget>[
           //Expanded es un widget que usa toda la pantalla disponible
           Expanded(
-            child: Center(
-              child: Container(
-                width: 140,
-                height: 140,
-                color: Colors.transparent,
-                child: RadialSeekBar(
-                  trackColor: Color(0xFFdddddd),
-                  progressPercent: 0.2,
-                  progressColor: accentColor,
-                  thumbPosition: 0.2,
-                  thumbColor: lightAccentColor,
-                  innerPadding: const EdgeInsets.all(7.0),
-                  outerPadding: const EdgeInsets.all(7.0),
-                  child: ClipOval(
-                    clipper: CircleClipper(),
-                    child: Image.network(
-                      demoPlaylist.songs[1].albumArtUrl,
-                      fit: BoxFit
-                          .cover, //fit para usar cierto tamaño, en este caso el espacio disponible
-                    ),
-                  ),
-                ),
-              ),
-            ),
+            //Aqui agregamos un Cntainer para darle la opcion que dentro de todo el espacio vacio
+            //se adelante o se atrase la cancion
+            child: new RadialSeekBar(),
           ),
           //Visualizador
           Container(
@@ -90,8 +72,104 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-//Esta clase se utiliza para mapear los componentes que tendra el SeekBar
 class RadialSeekBar extends StatefulWidget {
+  final double seekPercent;
+  RadialSeekBar({
+    this.seekPercent = 0.0,
+  });
+  @override
+  RadialSeekBarState createState() {
+    return new RadialSeekBarState();
+  }
+}
+
+class RadialSeekBarState extends State<RadialSeekBar> {
+  double _seekPercent = 0.0;
+  PolarCoord _startDragCoord;
+  double _startDreagPercent;
+  double _currentDragPercent;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _seekPercent = widget.seekPercent;
+  }
+
+  @override
+  void didUpdateWidget(RadialSeekBar oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    _seekPercent = widget.seekPercent;
+  }
+
+  void _onDragStart(PolarCoord coord) {
+    print("hola desde el inicio");
+    _startDragCoord = coord;
+    _startDreagPercent = _seekPercent;
+  }
+
+  void _onDragUpdate(PolarCoord coord) {
+    print("hola desde el update");
+    final dragAngle = coord.angle - _startDragCoord.angle;
+    final dragPercent = dragAngle / (2 * pi);
+    setState(() {
+      //%sirve para poner un poner porcentaje (normalizar)
+      _currentDragPercent = ((_startDreagPercent + dragPercent )% 1.0);
+    });
+  }
+
+  void _onDragEnd() {
+    print("hola desde el final");
+    setState(() {
+      _seekPercent = _currentDragPercent;
+      _currentDragPercent = null;
+      _startDragCoord = null;
+      _startDreagPercent = 0.0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new RadialDragGestureDetector(
+      onRadialDragStart: _onDragStart,
+      onRadialDragUpdate: _onDragUpdate,
+      onRadialDragEnd: _onDragEnd,
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.transparent,
+        //Es muy importante el poner el transparente pues asi se esconde el contenedor para poder arrastarar
+        child: Center(
+          child: Container(
+            width: 200.0,
+            height: 200.0,
+            child: new RadialProgressBar(
+              trackColor: Color(0xFFdddddd),
+              progressPercent: _currentDragPercent ?? _seekPercent,
+              progressColor: accentColor,
+              thumbPosition: _currentDragPercent ?? _seekPercent,
+              thumbColor: lightAccentColor,
+              innerPadding: const EdgeInsets.all(7.0),
+              outerPadding: const EdgeInsets.all(7.0),
+              child: ClipOval(
+                clipper: CircleClipper(),
+                child: Image.network(
+                  demoPlaylist.songs[1].albumArtUrl,
+                  fit: BoxFit.cover,
+                  //fit para usar cierto tamaño, en este caso el espacio disponible
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//Esta clase se utiliza para mapear los componentes que tendra el SeekBar
+//Para que sea una barra progresiva la vamos a cambiar a un RadialProgressBar
+class RadialProgressBar extends StatefulWidget {
   final double trackWidth;
   final Color trackColor;
   final double progressWidth;
@@ -107,7 +185,7 @@ class RadialSeekBar extends StatefulWidget {
   final Widget
       child; //Es el hijo que se dibuja dentro en este caso es la imagen del album
 
-  RadialSeekBar({
+  RadialProgressBar({
     this.trackWidth = 5,
     this.trackColor = Colors.grey,
     this.progressWidth = 5,
@@ -120,11 +198,11 @@ class RadialSeekBar extends StatefulWidget {
     this.innerPadding = const EdgeInsets.all(0.0),
     this.child,
   });
-  _RadialSeekBarState createState() => _RadialSeekBarState();
+  _RadialProgressBarState createState() => _RadialProgressBarState();
 }
 
 //Esta clase maneja los estados y ademas utiliza RadialSeekBarPainter para pintar el SeekBar
-class _RadialSeekBarState extends State<RadialSeekBar> {
+class _RadialProgressBarState extends State<RadialProgressBar> {
   //Variable para controlar que cuando la imagen del album sea grande se recorte entonces controlamos el padding
   EdgeInsets _insetsForPainter() {
     //Variable para el calculo de cual es el mas gruezo en base a eso le envia el padding correspondiente
